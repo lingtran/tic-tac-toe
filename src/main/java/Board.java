8fca4ecfd6a710e8045fd088ef9097cfe3dc2cb2
Ling Tran <lxiutran@gmail.com>
2017-01-13 15:06:15 -0600

    force pass initial thought on drawRow method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,12 +1,22 @@
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ltran on 1/13/17.
  */
 public class Board {
+    PrintStream printStream;
+
     public Board(PrintStream printStream) {
+        this.printStream = printStream;
     }
 
     public void draw() {
+        printStream.println("1|2|3\n-----\n4|5|6\n-----\n7|8|9\n");
+    }
+
+    public String drawRow() {
+        return "1|2|3";
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,7 +1,9 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -10,15 +12,27 @@ import static org.mockito.Mockito.verify;
  * Created by ltran on 1/13/17.
  */
 public class BoardTest {
+    private PrintStream printStream;
+    private Board board;
+
+    @Before
+    public void setUp() {
+        printStream = mock(PrintStream.class);
+        board = new Board(printStream);
+    }
+
     @Test
     public void shouldDrawBoard() {
-        PrintStream printStream = mock(PrintStream.class);
-        Board board = new Board(printStream);
-
         board.draw();
 
-        verify(printStream).println("1|2|3\n-----\n4|5|6\n-----7|8|9\n");
+        verify(printStream).println("1|2|3\n-----\n4|5|6\n-----\n7|8|9\n");
+    }
 
+    @Test
+    public void shouldDrawARowWithThreePositions(){
+        String result = board.drawRow();
+
+        assertThat(result, is("1|2|3"));
 
     }
 }
