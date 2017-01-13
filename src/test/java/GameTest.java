bc9b26569340b7ad5a126b994d06607133412a35
Ling Tran <lxiutran@gmail.com>
2017-01-13 16:57:48 -0600

    Refactor Game test to remove duplications

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -8,13 +9,21 @@ import static org.mockito.Mockito.*;
  * Created by ltran on 1/13/17.
  */
 public class GameTest {
+    private PrintStream printStream;
+    private Board board;
+    private Player playerOne;
+    private Game game;
+
+    @Before
+    public void setUp() {
+        printStream = mock(PrintStream.class);
+        board = mock(Board.class);
+        playerOne = mock(Player.class);
+        game = new Game(board, playerOne, printStream);
+    }
+
     @Test
     public void shouldHaveBoardAfterGameStarts() {
-        PrintStream printStream = mock(PrintStream.class);
-        Board board = mock(Board.class);
-        Player playerOne = mock(Player.class);
-        Game game = new Game(board, playerOne, printStream);
-
         game.start();
 
         verify(board).draw();
@@ -22,11 +31,6 @@ public class GameTest {
 
     @Test
     public void shouldAskPlayerToEnterAPositionNumber() {
-        PrintStream printStream = mock(PrintStream.class);
-        Board board = mock(Board.class);
-        Player playerOne = mock(Player.class);
-        Game game = new Game(board, playerOne, printStream);
-
         game.askPlayerWhereToMark();
 
         verify(printStream).println("\nTell me a number where you want to make a mark:");
